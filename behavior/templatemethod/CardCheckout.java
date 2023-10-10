8d255ba04e998e8db4c106ae6a8f3f30c0749c75
mia-developer <office.yeon@gmail.com>
2023-10-10 22:34:29 +0900

    feat: TemplateMethodPattern

---
+++ behavior/templatemethod/CardCheckout.java
@@ -0,0 +1,24 @@
+package behavior.templatemethod;
+
+public class CardCheckout extends Checkout {
+
+	private final String cardNumber;
+
+	public CardCheckout(final String cardNumber){
+		this.cardNumber = cardNumber;
+	}
+
+	@Override
+	void charge(final Long price, final Long paidAmount) {
+		System.out.println("charged by card");
+	}
+
+	@Override
+	protected void validate(final Long price, final Long paidAmount) throws IllegalAccessException {
+		super.validate(price, paidAmount);
+
+		if(cardNumber.length() > 100){
+			throw new IllegalAccessException();
+		}
+	}
+}
