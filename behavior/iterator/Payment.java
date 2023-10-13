4a4275635974af882791f250bc5675c7cf37cca8
mia-developer <office.yeon@gmail.com>
2023-10-13 23:35:16 +0900

    feat: IteratorPattern

---
+++ behavior/iterator/Payment.java
@@ -0,0 +1,13 @@
+package behavior.iterator;
+
+public class Payment {
+	private final Long amount;
+
+	public Payment(final Long amount) {
+		this.amount = amount;
+	}
+
+	public Long getAmount() {
+		return this.amount;
+	}
+}
---
+++ behavior/iterator/PaymentIterator.java
@@ -0,0 +1,28 @@
+package behavior.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class PaymentIterator implements Iterator<Payment> {
+
+	private final List<Payment> payments;
+	private int cursor;
+
+	public PaymentIterator(final List<Payment> payments) {
+		this.payments = payments;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return cursor < payments.size();
+	}
+
+	@Override
+	public Payment next() {
+		if (!hasNext()) {
+			throw new NoSuchElementException();
+		}
+		return payments.get(cursor++);
+	}
+}
